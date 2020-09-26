fd9d7e10f061a4a76426f46cbad703a16a6b54e2
musir88 <xyz1005@163.com>
2020-09-26 21:30:17 +0800

    初始化2020.09.26

---
+++ app/Http/Controllers/Getrich/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Getrich;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //
+
+    public function index()
+    {
+
+
+    	
+
+    	// 视图
+    	return view('getrich.index.index',[
+
+    	]);
+    }
+}
